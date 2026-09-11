e4ffd0e9426c224b5db5a502fa34078fec6a2e85
NathaliStender <126335082+NathaliStender@users.noreply.github.com>
2026-09-11 11:47:57 +0200

    Week 3 css

---
+++ Week-03/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import ToDoList from "./ToDoList.jsx";
+import { useState } from "react";
+
+function App() {
+  const annasToDoList = ["Call the landlord", "Book the dentist"];
+
+  const [name, setName] = useState("Anna");
+
+  return (
+    <>
+      <div className="main-inner">
+        <ToDoList firstName={name} />
+      </div>
+    </>
+  );
+}
+
+export default App;
